589ce231665f08504e112b4736ec6652b6f1a65e
azhar <azhartahir0100@gmail.com>
2016-06-09 22:59:43 +0500

    Designed the initial post request method to get the Result Html Page.

---
+++ post_request.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import requests
+
+url = 'http://result.biselahore.com/Home/Result'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+# params = { 'degree': 'SSC' , 'rollNum': '' , 'session': '2' , 'year': '2015' }
+
+def get_html_response(rollNum,degree,session,year):
+	params = { 'degree': degree , 'rollNum': rollNum , 'session': session , 'year': year }
+	return requests.post(url , data=params , headers=headers)
+	
