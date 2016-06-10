f84fbb7dec7c593cedf81fa9a6b2fd032df0818c
azhar <azharfamily786241@gmail.com>
2016-06-10 15:52:46 +0500

    Started work on getting result in dict form.
    The dict form refers to the output  of the get_result function , which is a dict.Currently the dict only contains the rollNum key but it will soon grow out of it.This is an unstable commit.

--- post_request.py
+++ post_request.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://result.biselahore.com/Home/Result'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -10,3 +11,9 @@ def get_html_response(rollNum,degree,session,year):
 	params = { 'degree': degree , 'rollNum': rollNum , 'session': session , 'year': year }
 	return requests.post(url , data=params , headers=headers)
 	
+def get_result(rollNum,degree,session,year):
+	html_response = get_html_response(rollNum,degree,session,year)
+	result_soup = BeautifulSoup(html_response.text)
+	result_dict = {
+		rollNum = result_soup.select(".td2  h5 > u")[0].string
+	}
