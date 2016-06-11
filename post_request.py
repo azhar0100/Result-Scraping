da9e8153e3c481b41e720dffe2c0c73e481fd798
azhar <azhartahir0100@gmail.com>
2016-06-11 13:02:02 +0500

    Added the registration number to the result_dict
    The registration number is available in the regNum key.Made some code changes and also added tests to check both the rollNum and regNum.

--- post_request.py
+++ post_request.py
@@ -14,8 +14,17 @@ def get_html_response(rollNum,degree,session,year):
 def get_result(rollNum,degree,session,year):
 	html_response = get_html_response(rollNum,degree,session,year)
 	result_soup = BeautifulSoup(html_response.text)
-	result_dict = {
-		"rollNum" : result_soup.select(".td2  h5 > u")[0].string.strip()
-	}
+	result_dict = {}
+
+	# The result page has two tables , one to the left and other to the right.
+	# The page uses an ancient table layout.The middle one has the actual data.
+	# The middle_table is divided into many <tr> elements which are handled separately.
+
+	middle_table = result_soup.select(".td2")[0].table
+	roll_and_reg_row = middle_table('tr',recursive=False)[1].td.table.tr
+	result_dict.update({
+		"rollNum" : roll_and_reg_row(True,recursive=False)[0].h5.u.string.strip() ,
+		"regNum"  : roll_and_reg_row(True,recursive=False)[2].p.u.string.strip()
+	})
 
 	return result_dict
--- tests/rollNumKey_test.py
+++ tests/rollNumKey_test.py
@@ -1,3 +1,5 @@
 from post_request import get_result
 
-assert get_result('170988','SSC','2','2015')['rollNum'] == '170988'
+result = get_result('170988','SSC','2','2015')
+assert result['rollNum'] == '170988'
+assert result['regNum']  == '10025-23961-2013'
