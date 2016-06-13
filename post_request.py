83bba4c40270a46b357e98d7edbc5af6b16cc52f
azhar <azharfamily786241@gmail.com>
2016-06-13 12:49:28 +0500

    Removed the function for of get_result

--- post_request.py
+++ post_request.py
@@ -28,46 +28,6 @@ def get_html_response(rollNum,degree,session,year):
 def get_tag_contents(bs4_tag):
 	"""Function to get the children of bs4_tag which are tags and not NavigableString"""
 	return bs4_tag(True,recursive=False)
-	
-def get_result(rollNum,degree,session,year):
-	html_response = get_html_response(rollNum,degree,session,year)
-	result_soup = BeautifulSoup(html_response.text)
-	result_dict = {}
-
-	# The result page has two tables , one to the left and other to the right.
-	# The page uses an ancient table layout.The middle one has the actual data.
-	# The middle_table is divided into many <tr> elements which are handled separately.
-
-	middle_table = result_soup.select(".td2")[0].table
-
-	reg_row = middle_table('tr',recursive=False)[1].td.table.tr
-	result_dict.update({
-		"rollNum" : get_tag_contents(reg_row)[0].h5.u.string.strip() ,
-		"regNum"  : get_tag_contents(reg_row)[2].p.u.string.strip()
-	})
-
-	degree_row = middle_table('tr',recursive=False)[2].select('h4')[0]
-	degree_and_exam_str = list(degree_row.stripped_strings)[0]
-	result_dict.update({
-		"degree"   : re.search(r'([^()]+)\(',degree_and_exam_str).groups()[0].strip() ,
-		"examType" : re.search(r'\(([^()]+)\)',degree_and_exam_str).groups()[0].strip() ,
-		"year"     : datetime.strptime(degree_row.u.string.strip(),"%Y").date() ,	
-		"group"    : degree_row.select('u')[1].string.strip()
-	})
-
-	credential_row = middle_table('tr',recursive=False)[3].table
-
-	result_dict.update({
-		"student_name" : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[0].find_all('td',recursive=False)[1])[0].string),
-		"father_name"  : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[1].find_all('td',recursive=False)[1])[0].string),
-		"centre"       : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[3].find_all('td',recursive=False)[1])[0].string)
-	})
-	result_dict["date_of_birth"] = datetime.strptime(get_tag_contents(credential_row.find_all('tr',recursive=False)[2].find_all('td',recursive=False)[1])[0].string,"%d/%m/%Y").date()
-
-	marks_row = middle_table('tr',recursive=False)[4].td
-	print marks_row
-
-	return result_dict
 
 class Result(object):
 
@@ -93,7 +53,7 @@ class Result(object):
 		return self.reg_row['regNum']
 
 	@lazy_property
-	def degree_row:
+	def degree_row(self):
 		degree_row = self.middle_table('tr',recursive=False)[2].select('h4')[0]
 		degree_and_exam_str = list(degree_row.stripped_strings)[0]
 		return {
@@ -120,34 +80,34 @@ class Result(object):
 		return self.degree_row['group']
 
 	@lazy_property
-	def credential_row:
-		credential_row = middle_table('tr',recursive=False)[3].table
+	def credential_row(self):
+		credential_row = self.middle_table('tr',recursive=False)[3].table
 
-	return {
-		"student_name" : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[0].find_all('td',recursive=False)[1])[0].string),
-		"father_name"  : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[1].find_all('td',recursive=False)[1])[0].string),
-		"centre"       : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[3].find_all('td',recursive=False)[1])[0].string)
-		"date_of_birth": datetime.strptime(get_tag_contents(credential_row.find_all('tr',recursive=False)[2].find_all('td',recursive=False)[1])[0].string,"%d/%m/%Y").date()
-	}
+		return {
+			"student_name" : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[0].find_all('td',recursive=False)[1])[0].string),
+			"father_name"  : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[1].find_all('td',recursive=False)[1])[0].string),
+			"centre"       : unicode(get_tag_contents(credential_row.find_all('tr',recursive=False)[3].find_all('td',recursive=False)[1])[0].string),
+			"date_of_birth": datetime.strptime(get_tag_contents(credential_row.find_all('tr',recursive=False)[2].find_all('td',recursive=False)[1])[0].string ,"%d/%m/%Y").date()
+		}
 
 	@lazy_property
-	def student_name:
+	def student_name(self):
 		return credential_row['student_name']
 
 	@lazy_property
-	def father_name:
+	def father_name(self):
 		return credential_row['father_name']
 
 	@lazy_property
-	def centre:
+	def centre(self):
 		return credential_row['centre']
 
 	@lazy_property
-	def date_of_birth:
+	def date_of_birth(self):
 		return credential_row['date_of_birth']
 
 	@lazy_property
-	def dict:
+	def dict(self):
 		result_dict = {}
 		result_dict.update(self.reg_row)
 		result_dict.update(self.degree_row)
--- tests/rollNumKey_test.py
+++ tests/rollNumKey_test.py
@@ -2,10 +2,7 @@ from post_request import get_result
 
 # {'regNum': u'10025-23961-2013', 'examType': u'ANNUAL', 'rollNum': u'170988', 'degree': u'SECONDARY SCHOOL CERTIFICATE ', 'year': u'2015', 'group': u'SCIENCE'} 
 
-result = get_result('170988','SSC','2','2015')
-assert result['rollNum'] == '170988'
-assert result['regNum']  == '10025-23961-2013'
-print result['degree']
-assert result['degree']  == 'SECONDARY SCHOOL CERTIFICATE'
-assert result['examType'] == 'ANNUAL'
-assert result['group'] == 'SCIENCE'
+expected = {'regNum': u'10025-23961-2013', 'examType': u'ANNUAL', 'father_name': u'TAHIR MEHMOOD', 'rollNum': u'170988', 'degree': u'SECONDARY SCHOOL CERTIFICATE', 'year': datetime.date(2015, 1, 1), 'date_of_birth': datetime.date(2000, 6, 30), 'group': u'SCIENCE', 'centre': u'ALLAMA IQBAL BOYS HIGH SCHOOL,  53-H CANAL BERG MULTAN ROAD LAHORE ', 'student_name': u'AZHAR TAHIR'}
+
+result = Result('170988','SSC','2','2015').dict
+assert result == expected
