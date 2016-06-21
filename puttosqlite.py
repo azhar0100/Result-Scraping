1607c4edaad587ad5bcacc1ddb3789401cf74ee3
azhar <azhartahir0100@gmail.com>
2016-06-21 20:43:13 +0500

    Changed file path to a copy and added status print.

--- puttosqlite.py
+++ puttosqlite.py
@@ -2,7 +2,7 @@
 
 import sqlite3
 
-rollNumFile = open('rollNumFile.txt','r')
+rollNumFile = open('rollNumFi.txt','r')
 conn = sqlite3.connect('rollNumFile.sqlite')
 c = conn.cursor()
 
@@ -12,6 +12,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS rollnums (
 rollnums = [(int(x[0]),x[1]) for x in [eval(x) for x in rollNumFile.readlines()]]
 
 for rollnum in rollnums:
+	print "On rollnum" , rollnum
 	c.execute('''INSERT INTO rollnums VALUES({rn},{st})'''.format(rn=rollnum[0],st=rollnum[1]))
 
 conn.commit()
