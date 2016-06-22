576d86979ae2b7503d00f31fdc8e833914ddf8d4
azhar <azhartahir0100@gmail.com>
2016-06-22 23:07:56 +0500

    Modified result_rollnum to also put the result.html in the return tuple.

--- get_result.py
+++ get_result.py
@@ -13,15 +13,16 @@ start_time = time()
 def call_result(arg_tuple):
 	return Result(*arg_tuple)
 
-def result_rollNum(arg_tuple):
+def get_result(arg_tuple):
 	try:
-		return (call_result(arg_tuple).rollNum,1) 
+		rslt = call_result(arg_tuple)
+		return (rslt.rollNum,1,rslt.html) 
 	except StudentNotFound as e:
-		return (arg_tuple[0],0)
+		return (arg_tuple[0],0,'')
 	except IOError as e:
-		return (arg_tuple[0],2)
+		return (arg_tuple[0],2,'')
 	except Exception as e:
-		return (arg_tuple[0],3)
+		return (arg_tuple[0],3,'')
 
 if __name__ == "__main__":
 	conn = sqlite3.connect('rollNumFile.sqlite')
@@ -32,7 +33,7 @@ if __name__ == "__main__":
 	print("Finished building ROll_NUM_LIST")
 	POOL_SIZE = 12
 	pool = Pool(POOL_SIZE)
-	results = pool.imap(result_rollNum,((str(x),'SSC','2','2015') for x in ROll_NUM_LIST))
+	results = pool.imap(get_result,((str(x),'SSC','2','2015') for x in ROll_NUM_LIST))
 	print('started')
 	for result in results:
 		try:
