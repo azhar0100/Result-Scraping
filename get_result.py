90e8e103d7c83fc5ef6fa8d8036a670ba76e8275
azhar <azhartahir0100@gmail.com>
2016-06-22 15:44:02 +0500

    Modified the avoid rollnum condition to only avoid matches.

--- get_result.py
+++ get_result.py
@@ -25,7 +25,7 @@ def result_rollNum(arg_tuple):
 if __name__ == "__main__":
 	with open('rollNumFile.txt','r') as f:
 		lines = f.readlines()
-		avoid_rollNums = [int(eval(x)[0]) for x in lines if not re.search(r'^\n$',x) and not int(eval(x)[1]) == 2 ]
+		avoid_rollNums = [int(eval(x)[0]) for x in lines if eval(x)[1] == 1 ]
 
 	print("Started building ROll_NUM_LIST")
 	ROll_NUM_LIST = [x for x in range(100000,200000) if  x not in avoid_rollNums]
