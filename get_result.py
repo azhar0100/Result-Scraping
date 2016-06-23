109b1c20757d39846e689f13d010ccd2fb67961a
azhar <azhartahir0100@gmail.com>
2016-06-23 13:01:21 +0500

    Added ignore only rows without empty html elements.

--- get_result.py
+++ get_result.py
@@ -27,7 +27,7 @@ def get_result(arg_tuple):
 if __name__ == "__main__":
 	conn = sqlite3.connect('rollNumFile.sqlite')
 	c = conn.cursor()
-	avoid_rollNums = set([x[0] for x in c.execute(r'''SELECT rollnum FROM rollnums WHERE status = 1 OR status = 0 OR html="" ''').fetchall()])
+	avoid_rollNums = set([x[0] for x in c.execute(r'''SELECT rollnum FROM rollnums WHERE html!="" ''').fetchall()])
 	print("Started building ROll_NUM_LIST")
 	ROll_NUM_LIST = [x for x in range(100000,200000) if  x not in avoid_rollNums]
 	print("Finished building ROll_NUM_LIST")
