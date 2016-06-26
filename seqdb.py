10d97a71eee5132f4ed1e1ff22a92e6a04b8a84f
azhar <azhartahir0100@gmail.com>
2016-06-26 16:41:54 +0500

    Added logging with custom logger.

--- seqdb.py
+++ seqdb.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 import sqlite3
+import logging
 
+logger = logging.getLogger(__name__)
+logger.basicConfig(format='%(asctime)s - %(name)s - %(message)s')
+
+logger.info("Started the {mod_name} ".format(__name__))
 conn = sqlite3.connect('rollNumFile.sqlite')
 c = conn.cursor()
-
+logger.info("Formed the database connection:{}".format(c))
 N = 2
 rollNumSeqConn = sqlite3.connect('rtgz/rollNumFile{}00k.sqlite'.format(N))
 rollNumSeqCurs = rollNumSeqConn.cursor()
+logger.info("Formed the database connection:{}".format(rollNumSeqCurs))
 rollNumSeqCurs.execute('''CREATE TABLE IF NOT EXISTS rollnums (
 	rollnum INTEGER PRIMARY KEY,
 	status INTEGER
 	html TEXT )''')
 
-rollnums = c.execute('''SELECT * FROM rollnums WHERE rollnum > 200000''').fetchall()
+rollnums = c.execute('''SELECT * FROM rollnums WHERE rollnum > 200000''')
 for rollnum in rollnums:
-	print rollnum[0:2]
+	logger.debug(rollnum[0:2])
 	rollNumSeqCurs.execute('''INSERT OR REPLACE INTO rollnums VALUES(?,?,?)''',(rollnum[0],rollnum[1],rollnum[2]))
