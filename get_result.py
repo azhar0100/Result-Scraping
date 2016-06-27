2a989335dc61dcdf71f1fe7d20072aec87bcefcb
azhar <azhartahir0100@gmail.com>
2016-06-27 20:18:08 +0500

    Added logging in get_result.

--- get_result.py
+++ get_result.py
@@ -7,7 +7,12 @@ from time import time
 from itertools import islice
 import re
 import sqlite3
+import logging
 start_time = time()
+logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger('get_result')
+file_handler = logging.FileHandler("rollNumFile.log")
+file_handler.setLevel(logging.DEBUG)
 
 def split_every(n, iterable):
     i = iter(iterable)
@@ -44,17 +49,18 @@ def shallow_query(cursor):
 
 if __name__ == "__main__":
 	conn = sqlite3.connect('rollNumFile.sqlite')
+	logger.info("Formed connection with the file:rollNumFile.sqlite")
 	c = conn.cursor()
 	avoid_rollNums = set([x[0] for x in deep_query(c)])
-	print( "SQLite query finished." , time()-start_time)
-	print("Started building ROll_NUM_LIST")
-	ROll_NUM_LIST = [x for x in range(100000,999999) if  x not in avoid_rollNums]
-	print("Finished building ROll_NUM_LIST")
+	logger.info("Formed the avoid_rollNums set")
+	logger.debug("The avoid_rollNums".format(avoid_rollNums))
+	ROLL_NUM_LIST = [x for x in range(100000,999999) if  x not in avoid_rollNums]
+	logger.info("Formed the ROLL_NUM_LIST")
+	logger.debug("The ROLL_NUM_LIST")
 	start_time = time()
 	POOL_SIZE = 100
 	pool = Pool(POOL_SIZE)
-	results = lazy_imap(get_result,[(str(x),'SSC','2','2015') for x in ROll_NUM_LIST],pool,100)
-	print('started')
+	results = lazy_imap(get_result,[(str(x),'SSC','2','2015') for x in ROLL_NUM_LIST],pool,100)
 	count = 0
 	for result in results:
 		count += 1
@@ -63,10 +69,10 @@ if __name__ == "__main__":
 		except sqlite3.IntegrityError:
 			c.execute(r'''UPDATE rollnums SET status = ?, html = ? WHERE rollnum=?''',(result[1],result[2],result[0]))
 		if count % 100 == 0:
-			print("Commit Now!")
+			logger.info("Commit Now at {}".format(count))
 			conn.commit()
-		print(result[0:2])
-	print( "========seconds=============" , time()-start_time)
+		logger.info(result[0:2])
+	logger.info( "========seconds============={}".format(time()-start_time))
 	pool.close()
 	pool.join()
 
