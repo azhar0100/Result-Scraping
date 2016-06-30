fc3ab04b665672f545200fd4d49ba12c979a802c
azhar <azhartahir0100@gmail.com>
2016-06-30 15:28:08 +0500

    Added log file.

--- scribble.py
+++ scribble.py
@@ -2,7 +2,7 @@
 import logging
 from result import Result
 
-logging.basicConfig('%(asctime)s - %(name)s - %(levelname)s - %(message)s', file='scribblelog.log')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', file='scribblelog.log')
 R = Result('171023','SSC','2','2015',html=open("../ReqResult.htm",'r').read())
 try:
 	rd = R.dict
