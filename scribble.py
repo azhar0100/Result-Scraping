c5950e1e4781747adf61ae32bf15e283489eb3ce
azhar <azhartahir0100@gmail.com>
2016-07-05 15:40:09 +0500

    Added better checking for problematic key.

--- scribble.py
+++ scribble.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import logging
 from result import Result
+import json
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', file='scribblelog.log')
+logging.basicConfig(level=logging.INFO)
 R = Result('109222','SSC','2','2015',html=open("../ReqResult.htm",'r').read())
 print R.dict
+attr_list = ['rollNum','regNum','student_name','father_name','centre','date_of_birth']
+print getattr(R,'rollNum')
+result_list = [getattr(R,x) for x in attr_list]
+result_list.append(json.dumps(R.marks_row))
+print (result_list[0:])
