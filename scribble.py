1d00ea99e6fae6c3794cc462ea2bb57a5c63b958
azhar <azhartahir0100@gmail.com>
2016-07-07 18:26:39 +0500

    Used getenv instead of providing environ using . as default value.

--- scribble.py
+++ scribble.py
@@ -3,14 +3,14 @@ import logging
 from result import Result
 import json
 import sqlite3
-from os import environ
+from os import getenv
 conn = sqlite3.connect('/home/azhar/db/rollNumFile.sqlite')
 
 logging.basicConfig(level=logging.INFO)
 
 def check_roll(rollnum):
 	R = Result(rollnum,'SSC','2','2015',html=conn.execute('''SELECT html FROM rollnums WHERE rollnum = {}'''.format(rollnum)).fetchone()[0])
-	with open("{}/{}.html".format(environ['TESTHTML'],rollnum),'w') as f:
+	with open("{}/{}.html".format(getenv('TESTHTML','.'),rollnum),'w') as f:
 		f.write(R.html)
 	print "The dict is :", R.dict
 	attr_list = ['rollNum','regNum','student_name','father_name','centre','date_of_birth']
