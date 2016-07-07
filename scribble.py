5c11c2296e3b2cd3fb6cfea8616cd94215f08c75
azhar <azhartahir0100@gmail.com>
2016-07-07 18:22:29 +0500

    + Added automatic put tested rollnums in $TESTHTML directory in scribble.py.

--- scribble.py
+++ scribble.py
@@ -3,12 +3,15 @@ import logging
 from result import Result
 import json
 import sqlite3
+from os import environ
 conn = sqlite3.connect('/home/azhar/db/rollNumFile.sqlite')
 
 logging.basicConfig(level=logging.INFO)
 
 def check_roll(rollnum):
 	R = Result(rollnum,'SSC','2','2015',html=conn.execute('''SELECT html FROM rollnums WHERE rollnum = {}'''.format(rollnum)).fetchone()[0])
+	with open("{}/{}.html".format(environ['TESTHTML'],rollnum),'w') as f:
+		f.write(R.html)
 	print "The dict is :", R.dict
 	attr_list = ['rollNum','regNum','student_name','father_name','centre','date_of_birth']
 	result_list = [R.dict[x] for x in attr_list]
