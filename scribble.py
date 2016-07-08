3b21346cb764a69f27712cc5cd6daac823677081
azhar <azhartahir0100@gmail.com>
2016-07-08 18:26:31 +0500

    Hoisted the saving of file to test-html.

--- scribble.py
+++ scribble.py
@@ -9,14 +9,15 @@ conn = sqlite3.connect(getenv('DBPATH'))
 logging.basicConfig(level=logging.INFO)
 
 def check_roll(rollnum):
-	R = Result(rollnum,'SSC','2','2015',html=conn.execute('''SELECT html FROM rollnums WHERE rollnum = {}'''.format(rollnum)).fetchone()[0])
+	page_html = conn.execute('''SELECT html FROM rollnums WHERE rollnum = {}'''.format(rollnum)).fetchone()[0]
 	with open("{}/{}.html".format(getenv('TESTHTML','.'),rollnum),'w') as f:
-		f.write(R.html)
+		f.write(page_html)
+	R = Result(rollnum,'SSC','2','2015',html=page_html)
 	print "The dict is :", R.dict
 	attr_list = ['rollNum','regNum','student_name','father_name','centre','date_of_birth']
 	result_list = [R.dict[x] for x in attr_list]
 	result_list.append(json.dumps(R.marks_row))
 	print (result_list[0:])
-rollnums = [100166,100101,115431]
+rollnums = [100166,100101,115431,284720,284721]
 for rollnum in rollnums:
 	check_roll(rollnum)
