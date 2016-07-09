c9b13ab92be7bf25c010dde543de38badeab804a
azhar <azhartahir0100@gmail.com>
2016-07-09 19:57:56 +0500

    Added initial changes for putting in a separate script.

--- get_result.py
+++ get_result.py
@@ -11,22 +11,17 @@ import sqlite3
 import json
 import logging
 start_time = time()
+
 logger = logging.getLogger('get_result')
-requests_logger = logging.getLogger('requests')
 logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 stream_handler.addFilter(logging.Filter("get_result"))
-file_handler = logging.FileHandler("rollNumFile.log")
-file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
-requests_logger.addHandler(file_handler)
-conf_path = 'get_result.json'
-logger.info("Reading conf from{}".format(conf_path))
+
 try:
 	with open(conf_path,'r') as f:
 		file_config = json.loads(f.read())
@@ -39,7 +34,7 @@ config.update(file_config)
 def call_result(arg_tuple):
 	return Result(*arg_tuple)
 
-def get_result(arg_tuple):
+def get_result_html(arg_tuple):
 	try:
 		rslt = call_result(arg_tuple)
 		return (rslt.rollNum,1,rslt.html) 
@@ -56,26 +51,44 @@ def deep_query(cursor):
 def shallow_query(cursor):
 	return cursor.execute(r'''SELECT rollnum FROM rolls WHERE status=0 OR status=1''').fetchall()
 
-if __name__ == "__main__":
-	conn = sqlite3.connect(config['DB_PATH'])
-	logger.info("Formed connection with the file:{}".format(config['DB_PATH']))
-	c = conn.cursor()
-	avoid_rollNums = set([x[0] for x in shallow_query(c)])
+def get_result(dbpath=None,
+	degree=None,
+	session=None,
+	year=None,
+	request_chunk_size=1000,
+	database_chunk_size=100,
+	pool_size=100,
+	log_dir='.'):
+	
+	file_handler = logging.FileHandler("{}/get_result.log".format(log_dir))
+	file_handler.setLevel(logging.DEBUG)
+	file_handler.setFormatter(formatter)
+	
+	conn = sqlite3.connect(dbpath)
+	conn.execute('''CREATE TABLE IF NOT EXISTS rollnums (
+	rollnum INTEGER PRIMARY KEY,
+	status INTEGER )''')
+	conn.execute('''CREATE TABLE IF NOT EXISTS resultHtml(
+		rollnum INTEGER PRIMARY KEY,
+		html TEXT
+		)''')
+	logger.info("Formed connection with the file:{}".format(dbpath))
+	avoid_rollNums = set([x[0] for x in shallow_query(conn)])
 	logger.info("Formed the avoid_rollNums set")
 	ROLL_NUM_LIST = [x for x in range(000000,999999) if  x not in avoid_rollNums]
 	logger.info("Formed the ROLL_NUM_LIST")
-	ROLL_NUM_TUPLE_LIST= [(str(x).zfill(6),config['DEGREE'],str(config['PART']),str(config['YEAR'])) for x in ROLL_NUM_LIST]
+	ROLL_NUM_TUPLE_LIST= [(str(x).zfill(6),degree,session,year) for x in ROLL_NUM_LIST]
 	start_time = time()
-	pool = Pool(config['POOL_SIZE'])
-	results = lazy_imap(get_result,ROLL_NUM_TUPLE_LIST,pool,config['REQUEST_CHUNK_SIZE'])
+	pool = Pool(pool_size)
+	results = lazy_imap(get_result_html,ROLL_NUM_TUPLE_LIST,pool,request_chunk_size)
 	count = 0
 	for result in results:
 		count += 1
 		c.execute(r'''INSERT OR REPLACE INTO rollnums VALUES(?,?,?)''',result)
 		c.execute(r'''INSERT OR REPLACE INTO rolls VALUES(?,?)''',result[0:2])
 		if result[1] == 2:
-			ROLL_NUM_TUPLE_LIST.append((str(result[0]),'SSC','2','2015'))
-		if count % config['DATABASE_CHUNK_SIZE'] == 0:
+			ROLL_NUM_TUPLE_LIST.append((str(result[0]),degree,session,year))
+		if count % database_chunk_size == 0:
 			logger.info("Commit Now at {}".format(count))
 			conn.commit()
 		logger.info(result[0:2])
