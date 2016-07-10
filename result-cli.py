2aee5e5badcb727ba167ee1f2c350ef944f3a99b
azhar <azhartahir0100@gmail.com>
2016-07-10 11:19:15 +0500

    Added the collect_result portion in result-cli.py

--- result-cli.py
+++ result-cli.py
@@ -6,13 +6,17 @@ def call_get_result(args):
 	arglist = ['dbpath','degree','session','year','request_chunk_size','database_chunk_size','pool_size']
 	get_result(**{ k:getattr(args,k) for k in arglist })
 
+def collect_get_result(args):
+	arglist = ['dbpath','degree','session','year']
+	collect_result(**{k:getattr(args,k) for k in arglist})
+
 parser = configargparse.Parser()
 parser.add('-l','--log-dir',help='The directory to put log files in.')
 
 subparsers= parser.add_subparsers()
 
 parser_get = subparsers.add_parser('get',help='This command fetches the data from the net.')
-parser_get.add('-c','--conf-file',required=True,is_config_file=True)
+parser_get.add('-c','--conf-file',is_config_file=True)
 parser_get.add('--degree',required=True)
 parser_get.add('--session',required=True)
 parser_get.add('--year',required=True,type=int)
@@ -21,6 +25,14 @@ parser_get.add('--database-chunk-size',type=int,default=100)
 parser_get.add('--pool-size',type=int,default=100)
 parser_get.add('--dbpath',required=True)
 parser_get.set_defaults(func=call_get_result)
+
+parser_collect = subparsers.add_parser('collect',help='This command fetches the data from the net.')
+parser_collect.add('-c','--conf-file',is_config_file=True)
+parser_collect.add('--dbpath',required=True)
+parser_collect.add('--degree',required=True)
+parser_collect.add('--session',required=True)
+parser_collect.add('--year',required=True,type=int)
+parser_collect.set_defaults(func=call_collect_result)
 
 args = parser.parse_args()
 args.func(args)
