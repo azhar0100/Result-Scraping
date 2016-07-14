d40808e2714065089c6f007018961f0ddecd0ddf
azhar <azhartahir0100@gmail.com>
2016-07-14 16:33:20 +0500

    Removed impossible params so that the default value can be used.

--- result-cli.py
+++ result-cli.py
@@ -4,7 +4,8 @@ from get_result import get_result
 from collect_result import collect_result
 
 def call_get_result(args):
-	arglist = ['dbpath','degree','session','year','request_chunk_size','database_chunk_size','pool_size']
+	# arglist = ['dbpath','degree','session','year','request_chunk_size','database_chunk_size','pool_size']
+	arglist = ['dbpath','year','request_chunk_size','database_chunk_size','pool_size']
 	get_result(**{ k:getattr(args,k) for k in arglist })
 
 def call_collect_result(args):
@@ -18,8 +19,8 @@ subparsers= parser.add_subparsers()
 
 parser_get = subparsers.add_parser('get',help='This command fetches the data from the net.')
 parser_get.add('-c','--conf-file',is_config_file=True)
-parser_get.add('--degree',required=True)
-parser_get.add('--session',required=True)
+parser_get.add('--degree')
+parser_get.add('--session')
 parser_get.add('--year',required=True,type=int)
 parser_get.add('--request-chunk-size',type=int,default=1000)
 parser_get.add('--database-chunk-size',type=int,default=100)
