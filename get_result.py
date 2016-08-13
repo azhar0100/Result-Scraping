6738a009565556f9afd99ad2656bf1f41f42f700
azhar <azhartahir0100@gmail.com>
2016-08-13 17:40:32 +0500

    Backported get_result.py script.

--- get_result.py
+++ get_result.py
@@ -47,18 +47,12 @@ def shallow_query(cursor):
 def get_result(dbpath=None,
 	degree=['SSC','HSSC'],
 	session=range(3),
-	year=None,
+	year=[2015],
 	request_chunk_size=1000,
 	database_chunk_size=100,
-	pool_size=100,
-	log_dir='.'):
+	pool_size=100):
 
 	degree_ints = {'SSC':1,'HSSC':2}
-	
-	file_handler = logging.FileHandler("{}/get_result.log".format(log_dir))
-	file_handler.setLevel(logging.DEBUG)
-	file_handler.setFormatter(formatter)
-	logger.addHandler(file_handler)
 
 	logger.info("Trying to form connection to file:{}".format(dbpath))
 	conn = sqlite3.connect(dbpath)
@@ -85,35 +79,54 @@ def get_result(dbpath=None,
 		rollnum INTEGER PRIMARY KEY,
 		html TEXT UNIQUE
 		)''')
+	for current_year in years:
+		for current_degree in degree:
+			conn.execute('''INSERT OR IGNORE INTO degrees VALUES(?,?)''',{v:k for (k,v) in degree_ints.items() if k == current_degree}.items()[0])
+			for current_session in session:
+				avoid_rollNums = set([x for (x,) in conn.execute(r'''SELECT rollnum FROM rollStatus WHERE status=1 OR (status=0 AND degree = ? AND session = ?)''',(degree_ints[current_degree],current_session))])
+				logger.info("Formed the avoid_rollNums set")
+				ROLL_NUM_LIST = [(x,current_degree,current_session,current_year) for x in (x for x in range(000000,999999) if  x not in avoid_rollNums)]
+				logger.info("Formed the ROLL_NUM_LIST")
+				pool = Pool(pool_size)
+				results = lazy_imap(get_result_html,ROLL_NUM_LIST,pool,request_chunk_size,ordered=False)
+				count = 0
+				for result in results:
+					count += 1
+					conn.execute(r'''
+						INSERT OR  REPLACE INTO rollStatus
+						SELECT {0} , d.id , {2} , {3} FROM degrees d
+						WHERE d.degree_name = '{1}'
+						'''.format(*(result[0:3] + (result[4],))))
+					if result[1] == 1:
+						conn.execute(r'''INSERT OR REPLACE INTO resultHtml VALUES(?,?)''',tuple((result[x] for x in [0,-1])))
+					if result[1] == 2:
+						ROLL_NUM_LIST.append(result[0],current_degree,current_session,year)
+					if count % database_chunk_size == 0:
+						logger.info("Commit Now at {}".format(count))
+						conn.commit()
+					logger.info(result[0:-1])
+				pool.close()
+				pool.join()
 
-	for current_degree in degree:
-		conn.execute('''INSERT OR IGNORE INTO degrees VALUES(?,?)''',{v:k for (k,v) in degree_ints.items() if k == current_degree}.items()[0])
 
-		for current_session in session:
-			avoid_rollNums = set([x for (x,) in conn.execute(r'''SELECT rollnum FROM rollStatus WHERE status=1 OR (status=0 AND degree = ? AND session = ?)''',(degree_ints[current_degree],current_session))])
-			logger.info("Formed the avoid_rollNums set")
-			ROLL_NUM_LIST = [(x,current_degree,current_session,year) for x in (x for x in range(000000,999999) if  x not in avoid_rollNums)]
-			logger.info("Formed the ROLL_NUM_LIST")
-			start_time = time()
-			pool = Pool(pool_size)
-			results = lazy_imap(get_result_html,ROLL_NUM_LIST,pool,request_chunk_size,ordered=False)
-			count = 0
-			for result in results:
-				count += 1
-				conn.execute(r'''
-					INSERT OR  REPLACE INTO rollStatus
-					SELECT {0} , d.id , {2} , {3} FROM degrees d
-					WHERE d.degree_name = '{1}'
-					'''.format(*(result[0:3] + (result[4],))))
-				if result[1] == 1:
-					conn.execute(r'''INSERT OR REPLACE INTO resultHtml VALUES(?,?)''',tuple((result[x] for x in [0,-1])))
-				if result[1] == 2:
-					ROLL_NUM_LIST.append(result[0],current_degree,current_session,year)
-				if count % database_chunk_size == 0:
-					logger.info("Commit Now at {}".format(count))
-					conn.commit()
-				logger.info(result[0:-1])
-			logger.info( "========seconds============={}".format(time()-start_time))
-			pool.close()
-			pool.join()
+if __name__ == '__main__':	
+	parser = configargparse.Parser()
 
+	parser.add('-l','--log-dir',help='The directory to put log files in.')
+	parser.add('-c','--conf-file',is_config_file=True)
+	parser.add('--degree', action='append')
+	parser.add('--session', action='append')
+	parser.add('--year',required=True,type=int, action='append')
+	parser.add('--request-chunk-size',type=int,default=1000)
+	parser.add('--database-chunk-size',type=int,default=100)
+	parser.add('--pool-size',type=int,default=100)
+	parser.add('--dbpath',required=True ,help='The path where the database file will be stored')
+	args = parser.parse_args()
+
+	file_handler = logging.FileHandler("{}/get_result.log".format(args.log_dir))
+	file_handler.setLevel(logging.DEBUG)
+	file_handler.setFormatter(formatter)
+	logger.addHandler(file_handler)
+
+	arglist = ['dbpath','degree','year','request_chunk_size','database_chunk_size','pool_size']
+	get_result(**{ k:getattr(args,k) for k in arglist })
